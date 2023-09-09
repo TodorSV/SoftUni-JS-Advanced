afb81bae617ab44784f0b3d529125c4549cbc6ab
Todor-Ivanov-Slavov <122822038+Todor-Ivanov-Slavov@users.noreply.github.com>
2023-09-09 17:43:29 +0300

    Update evenPositionElements.js

--- 01 Arrays and Nested Array/01 Arrays and Nested Arrays - Lab/evenPositionElements.js
+++ 01 Arrays and Nested Array/01 Arrays and Nested Arrays - Lab/evenPositionElements.js
@@ -6,7 +6,7 @@ function evenPosition(input) {
 }
 evenPosition(['20', '30', '40', '50', '60'])
 
-//Друг Вариант  ne m,oga da q ponqsqm tazi huinq 
+//Друг Вариант 
 
 /*
 function evenPosition(input) {
